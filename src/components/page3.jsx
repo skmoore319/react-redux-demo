18c96325e2c0d90d9460adc3aa31fe98a5e62ec2
Bino Philip <binophilip123@gmail.com>
2018-06-17 14:22:12 -0400

    my profile

---
+++ src/components/page3.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+class Page3Component extends Component {
+  render = () => (
+    <div>
+      <h1>Profile</h1>
+      <p>Name: Bino</p>
+      <p>Age: 24</p>
+      <p>Location: Florida</p>
+    </div>
+  );
+}
+
+export default Page3Component;
