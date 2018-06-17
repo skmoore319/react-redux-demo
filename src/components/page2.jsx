9f8243c49e2739f417efadadfb9434768b951bf6
Scott <sm.scottkmoore@gmail.com>
2018-06-17 14:13:09 -0400

    Add Page2Component to components

---
+++ src/components/page2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Page2Component extends React.Component{
+    render() {
+        return (
+            <h1>This is Page 2!</h1>
+        )
+    }
+}
+
+export default Page2Component;
