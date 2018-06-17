74315f251ab645d28e35dd063cd0760834cc73c7
Scott <sm.scottkmoore@gmail.com>
2018-06-17 14:18:01 -0400

    created profile page (Page 2)

--- src/components/page2.jsx
+++ src/components/page2.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 class Page2Component extends React.Component{
     render() {
         return (
-            <h1>This is Page 2!</h1>
+            <div>
+                <h1>This is Page 2!</h1>
+                <p>I am Scott</p>
+                <p>27</p>
+            </div>
         )
     }
 }
