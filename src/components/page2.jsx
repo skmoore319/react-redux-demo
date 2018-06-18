3a758bb0323bc1a79e6addcae35c400a09f96188
Scott <sm.scottkmoore@gmail.com>
2018-06-18 09:32:48 -0400

    Nested a component inside of Page 2

--- src/components/page2.jsx
+++ src/components/page2.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Page2A from './page2a';
 
 class Page2 extends Component {
     state = {
@@ -12,6 +13,7 @@ class Page2 extends Component {
       <h1>This is Page 2!</h1>
       <p>I am Scott</p>
       <p>27</p>
+      <Page2A />
     </div>
   )
 }
---
+++ src/components/page2a.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Page2A extends Component {
+    // state = {
+    //     name: '',
+    //     age: 0,
+    //     location: ''
+    // }
+
+  render = () => (
+    <div>
+      <h2>This component... is nested inside of Page 2!</h2>
+    </div>
+  )
+}
+
+export default Page2A
